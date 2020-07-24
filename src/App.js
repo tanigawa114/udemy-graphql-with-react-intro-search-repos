a6061866080f9cab13ef59d7273a1c894afd890e
tanigawa114 <tanigawa.8987@gmail.com>
2020-07-24 16:04:05 +0900

    show search results

--- src/App.js
+++ src/App.js
@@ -54,7 +54,23 @@ class App extends Component {
             const reositoryUnit =
               repositoryCount === 1 ? 'Repository' : 'Repositories';
             const title = `GitHub Repositories Search Results - ${repositoryCount} ${reositoryUnit}`;
-            return <h2>{title}</h2>;
+            return (
+              <>
+                <h2>{title}</h2>
+                <ul>
+                  {search.edges.map((edge) => {
+                    const node = edge.node;
+                    return (
+                      <li key={node.id}>
+                        <a href={node.url} target="_blank">
+                          {node.name}
+                        </a>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </>
+            );
           }}
         </Query>
       </ApolloProvider>
